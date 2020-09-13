e81b2bb89e27fabbff8c7a97321ae388dce54fdd
Crowdbotics <team@crowdbotics.com>
2020-09-13 23:07:44 +0000

    add package blueprint-react-native-tutorial-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Tutorial4101062', human_name: 'Tutorial 4', access_route: 'Tutorial4101062', icon: 'question-circle'},
 { name: 'Dashboard15101061', human_name: 'Dashboard 1 5', access_route: 'Dashboard15101061'},
 
   // you can add more installed blueprints here
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Tutorial4101062Navigator from '../features/Tutorial4101062/navigator';
 import Dashboard15101061Navigator from '../features/Dashboard15101061/navigator';
 
 /**
@@ -14,6 +15,7 @@ import Dashboard15101061Navigator from '../features/Dashboard15101061/navigator'
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+Tutorial4101062: { screen: Tutorial4101062Navigator },
 Dashboard15101061: { screen: Dashboard15101061Navigator },
 
     /** new navigators can be added here */
