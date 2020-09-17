61fdca47200b5341e3255ab704152c6274a938f1
Crowdbotics <team@crowdbotics.com>
2020-09-17 22:23:28 +0000

    add package blueprint-react-native-achievement-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Achievements10103256', human_name: 'Achievements 10', access_route: 'Achievements10103256'},
 { name: 'SignIn42101064', human_name: 'Sign In 4 2', access_route: 'SignIn42101064'},
 { name: 'UserProfile3101063', human_name: 'User Profile 3', access_route: 'UserProfile3101063'},
 { name: 'Tutorial4101062', human_name: 'Tutorial 4', access_route: 'Tutorial4101062', icon: 'question-circle'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Achievements10103256Navigator from '../features/Achievements10103256/navigator';
 import SignIn42101064Navigator from '../features/SignIn42101064/navigator';
 import UserProfile3101063Navigator from '../features/UserProfile3101063/navigator';
 import Tutorial4101062Navigator from '../features/Tutorial4101062/navigator';
@@ -17,6 +18,7 @@ import Dashboard15101061Navigator from '../features/Dashboard15101061/navigator'
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+Achievements10103256: { screen: Achievements10103256Navigator },
 SignIn42101064: { screen: SignIn42101064Navigator },
 UserProfile3101063: { screen: UserProfile3101063Navigator },
 Tutorial4101062: { screen: Tutorial4101062Navigator },
